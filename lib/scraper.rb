c600dfa8f2ccba450810bcb5e71246bb411d2500
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-24 21:40:07 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,7 +17,7 @@ class Scraper
       course.description = post.css("p").text
     end
   end
-binding 'pry'
+binding.pry
   def print_courses
     self.make_courses
     Course.all.each do |course|
